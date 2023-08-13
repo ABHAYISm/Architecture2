2560f54ffef04d34701cd3bb06a6eae9addfc4fd
Abhay <21je0013@iitism.ac.in>
2023-08-13 17:44:49 +0530

    new

--- app/src/main/java/com/example/learningarchitecture2/MainActivity.kt
+++ app/src/main/java/com/example/learningarchitecture2/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.learningarchitecture2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.learningarchitecture2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val quoteObj=Quote("Dont die in IIT","~Abhay")
+        binding.quote = quoteObj
+//        binding.Author.txt="~Abhay"
+//        binding.Quote.text="dont die in IIT"
     }
 }
---
+++ app/src/main/java/com/example/learningarchitecture2/Quote.kt
@@ -0,0 +1,3 @@
+package com.example.learningarchitecture2
+
+data class Quote(val text:String,val Author:String)
